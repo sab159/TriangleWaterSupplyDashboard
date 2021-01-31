b45bbd61f1bc814161c8f22551d604d1057151a4
ksonda <44071350+ksonda@users.noreply.github.com>
2021-01-30 21:00:55 -0500

    Create R-Docker-Post-test.R

---
+++ R/R-Docker-Post-test.R
@@ -0,0 +1,27 @@
+###
+endpoint <- "https://sta-demo.internetofwater.dev/api/v1.1/Datastreams(119)/"
+user <- "iow"
+pw <- "nieps"
+
+
+PostTestObs <- function(api, user, password,
+                    result,
+                    resultTime){
+  o <- jsonlite::toJSON(list(
+    result = result,
+    resultTime = resultTime,
+    phenomenonTime = resultTime
+  ), auto_unbox=TRUE)
+  
+  httr::POST(url = paste0(api,"Observations"), encode="json", httr::authenticate(user, password, type ="basic"),
+       body = o)
+  
+  #return(o)
+}
+
+PostTestObs(endpoint, 
+            user=user,
+            password=pw,
+            result=runif(1),
+            resultTime=parsedate::format_iso_8601(Sys.time()) )  
+  
