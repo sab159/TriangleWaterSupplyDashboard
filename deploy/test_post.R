052a4c34491a43d272f7d371cb15c6847ba258b5
ksonda <44071350+ksonda@users.noreply.github.com>
2021-02-09 11:37:03 -0500

    update

---
+++ deploy/test_post.R
@@ -0,0 +1,32 @@
+###
+# httr
+# parsedate
+#jsonlite
+endpoint <- "https://sta-demo.internetofwater.dev/api/v1.1/Datastreams(119)/"
+user <- "iow"
+pw <- "nieps"
+
+setwd("/src")
+PostTestObs <- function(api, user, password,
+                        result,
+                        resultTime){
+  o <- jsonlite::toJSON(list(
+    result = 200,
+    resultTime = resultTime,
+    phenomenonTime = resultTime
+  ), auto_unbox=TRUE)
+  
+  httr::POST(url = paste0(api,"Observations"), encode="json", httr::authenticate(user, password, type ="basic"),
+             body = o)
+  
+  #return(o)
+}
+
+PostTestObs(endpoint, 
+            user=user,
+            password=pw,
+            result=runif(1),
+            resultTime=parsedate::format_iso_8601(Sys.time()) )  
+
+x <- data.frame(c(endpoint, as.character(Sys.time())))
+write.csv(x,"x.csv")
